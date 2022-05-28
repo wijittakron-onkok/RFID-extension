6268f0fcae46fe5423f36be97402ebb725da8b56
Wijittakron Onkok <wijittakron@taladwaree.ac.th>
2022-05-28 20:53:58 +0700

    1.0.1

--- extension.js
+++ extension.js
@@ -1,9 +1,9 @@
 ({
     name: "RFID", // Category Name
-    description: "RFID reader 13.56MHz (MFRC522) SPI; SET Pin => SDA=SDA1, SCK=SLC1, MOSI=Pin18, MISO=Pin19",
+    description: "RFID reader 13.56MHz (MFRC522) SPI",
     author: "WIJITTAKRON",
     category: "Sensors",
-    version: "1.0.0",
+    version: "1.0.1",
     icon: "/static/icon.png", // Category icon
     color: "#3498DB", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
