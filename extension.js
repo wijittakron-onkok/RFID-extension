9fcfb893459c9866c7978c8b73ab4373af65e245
Wijittakron Onkok <wijittakron@taladwaree.ac.th>
2022-05-28 23:01:32 +0700

    1.0.2

--- extension.js
+++ extension.js
@@ -3,11 +3,10 @@
     description: "RFID reader 13.56MHz (MFRC522) SPI",
     author: "WIJITTAKRON",
     category: "Sensors",
-    version: "1.0.1",
+    version: "1.0.2",
     icon: "./static/icon.png", 
     color: "#3498DB", 
     blocks: [ 
-        "rfid_is_detected",
         "rfid_read_uid"
     ]
 });
