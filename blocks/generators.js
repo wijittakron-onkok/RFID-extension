9a66a4282319d803ee8616a263c8522ae3c5c9dd
Wijittakron Onkok <wijittakron@taladwaree.ac.th>
2022-08-01 20:27:33 +0700

    test

---
+++ blocks/generators.js
@@ -0,0 +1,11 @@
+Blockly.Python['dec_i2c_scanner'] = function(block) {
+  Blockly.Python.definitions_['import_kidbright_i2c'] = 'import KidBright_I2C as kb_i2c';
+
+  var dropdown_scl = block.getFieldValue('scl');
+  var dropdown_sda = block.getFieldValue('sda');
+  var value_freq = Blockly.Python.valueToCode(block, 'freq', Blockly.Python.ORDER_ATOMIC);
+
+  var code = `kb_i2c.decimal(${dropdown_scl}, ${dropdown_sda}, ${value_freq})`;
+
+  return [code, Blockly.Python.ORDER_NONE];
+};
---
+++ extension.js
@@ -0,0 +1,24 @@
+({
+  name: "I2C Scanner",
+  description: "KidBright V1.3 I2C Scanner",
+  author: "WIJITTAKRON",
+  category: "Other",
+  version: "1.0.0",
+  icon: "/static/icon.png",
+  color: "#8D44AD",
+  blocks: [
+    {
+      xml: `
+              <block type="dec_i2c_scanner">
+                  <field name="scl">22</field>
+                  <field name="sda">21</field>
+                  <value name="freq">
+                        <shadow type="math_number">
+                            <field name="NUM">100000</field>
+                        </shadow>
+                    </value>
+              </block>
+            `
+    }
+  ]
+});
