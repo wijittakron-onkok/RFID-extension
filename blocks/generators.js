69d92d8df0e0fef63d2bc59bd494d072fb68a475
Wijittakron Onkok <wijittakron@taladwaree.ac.th>
2022-08-01 18:23:56 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,23 +1,14 @@
-Blockly.JavaScript['kb_i2c_decimal'] = function (block) {
-  Blockly.Python.definitions_['import_kidbright_i2c'] = 'import kidbright_i2c as kb_i2c';
-
-  var dropdown_scl = block.getFieldValue('scl');
-  var dropdown_sda = block.getFieldValue('sda');
-  var number_freq = Blockly.Python.valueToCode(block, 'freq', Blockly.Python.ORDER_NONE);
+Blockly.JavaScript['kb_i2c_decimal'] = function () {
 
   var code = `kb_i2c`;
 
-  return [code, Blockly.Python.ORDER_NONE];
+  return code;
 };
 
-Blockly.JavaScript['kb_i2c_hexadecimal'] = function (block) {
-  Blockly.Python.definitions_['import_kidbright_i2c'] = 'import kidbright_i2c as kb_i2c';
+Blockly.JavaScript['kb_i2c_hexadecimal'] = function () {
 
-  var dropdown_scl = block.getFieldValue('scl');
-  var dropdown_sda = block.getFieldValue('sda');
-  var number_freq = Blockly.Python.valueToCode(block, 'freq', Blockly.Python.ORDER_NONE);
 
   var code = `kb_i2c`;
 
-  return [code, Blockly.Python.ORDER_NONE];
+  return code;
 };
