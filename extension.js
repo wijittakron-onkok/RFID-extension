46d50da29c4c05d6c2c625287dd4cda50eca8c3e
Wijittakron Onkok <wijittakron@taladwaree.ac.th>
2022-08-01 17:05:25 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -7,27 +7,7 @@
   icon: "/static/icon.png",
   color: "#985BA5",
   blocks: [
-    {
-      xml: `
-              <block type="kb_i2c_scanner_decimal">
-                  <value name="freq">
-                      <shadow type="math_number">
-                          <field name="NUM">100000</field>
-                      </shadow>
-                  </value>
-              </block>
-            `
-    },
-    {
-      xml: `
-              <block type="kb_i2c_scanner_hexadecimal">
-                  <value name="freq">
-                      <shadow type="math_number">
-                          <field name="NUM">100000</field>
-                      </shadow>
-                  </value>
-              </block>
-            `
-    }
+      "kb_i2c_scanner_decimal",
+      "kb_i2c_scanner_hexadecimal"
   ]
 });
